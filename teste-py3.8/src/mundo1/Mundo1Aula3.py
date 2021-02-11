c7aa4c87873b516f5885dffaf1ba163540d183d6
NicolasMCP <nicolas.mcp@gmail.com>
2021-02-11 12:47:48 -0300

    codigo desenvolvido enquanto assisto o Guanabara

---
+++ teste-py3.8/src/mundo1/Mundo1Aula3.py
@@ -0,0 +1,8 @@
+# Nícolas Ramos
+# desenvolvido enquanto assistia aula 3 do Mundo 1
+
+nome = input('Qual é o seu nome ? ')
+idade = input('Quantos anos você tem ? ')
+peso = input('Informe quel o seu peso ')
+print(f'Olá {nome}, então você têm {idade} anos e pesa {peso} kilos ?')
+print("Você tem certeza ???")
