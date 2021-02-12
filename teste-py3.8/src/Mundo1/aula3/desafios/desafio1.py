5db8632ac3f4471ed6e25fac5fbad40fb1656d3e
NicolasMCP <nicolas.mcp@gmail.com>
2021-02-12 08:32:39 -0300

    desafio pedido

--- teste-py3.8/src/Mundo1/aula3/desafios/desafio1.py
+++ teste-py3.8/src/Mundo1/aula3/desafios/desafio1.py
@@ -1,3 +1,6 @@
+# Nícolas Ramos
+# desenvolvido para ser igual ao pedido no desafio
+
 nome = input('Qual é o seu nome ? ')
 
 print(f'Olá {nome}! Prazer em te conhecer!')
---
+++ teste-py3.8/src/Mundo1/aula3/desafios/desafio2.py
@@ -0,0 +1,8 @@
+# Nícolas Ramos
+# desenvolvido para ser igual ao pedido no desafio
+
+dia = input('Dia = ')
+mes = input('Mes = ')
+ano = input('Ano = ')
+
+print(f'Você nasceu no dia {dia} de {mes} de {ano}. Correto ?')
