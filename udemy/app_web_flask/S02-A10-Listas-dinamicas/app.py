5f88ae9332ed13bc1eee2283c1ddc29f17fe4ef9
NicolasMCP <nicolas.mcp@gmail.com>
2022-08-15 16:11:00 -0300

    lista dinâmica

---
+++ udemy/app_web_flask/S02-A10-Listas-dinamicas/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+frutas = []
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        if request.form.get('fruta'):
+            frutas.append(request.form.get('fruta'))
+    return render_template('lista_dinamica.html', frutas=frutas)
+
+
+if __name__ == '__main__':
+    app.run()
