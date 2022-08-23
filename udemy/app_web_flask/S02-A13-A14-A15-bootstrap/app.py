484d6e0c660876320369de680cfafe3bb5d242fa
NicolasMCP <nicolas.mcp@gmail.com>
2022-08-22 23:40:25 -0300

    Colocando bootstrap e melhorando layout...

---
+++ udemy/app_web_flask/S02-A13-A14-A15-bootstrap/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+frutas = []
+registros = []
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        if request.form.get("fruta"):
+            frutas.append(request.form.get("fruta"))
+    return render_template("frutas.html", frutas=frutas)
+
+
+@app.route("/alunos", methods=["GET", "POST"])
+def alunos():
+    if request.method == "POST":
+        if request.form.get("aluno") and request.form.get("nota"):
+            registros.append({"aluno": request.form.get("aluno"),
+                              "nota": request.form.get("nota")
+                              })
+    return render_template("alunos.html", registros=registros)
+
+
+if __name__ == "__main__""":
+    app.run(debug=True)
