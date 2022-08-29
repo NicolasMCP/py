d77f595361a076b92500d5e85f8aafa19714af48
Nicolas Ramos <nicolas.mcp@gmail.com>
2022-08-29 00:04:23 -0300

    minuto estranho, exemplo 02

---
+++ lv-use-a-cabeca/cap_01b-odd2_p35.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import random
+import time
+
+estranhos = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
+             21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
+             41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
+
+for i in range(5):
+    minuto_atual = datetime.today().minute
+    if minuto_atual in estranhos:
+        print("Este é um miuto um pouco estranho.")
+    else:
+        print("Não, este não é um minuto estranho.")
+    aguarde_segundos = random.randint(10, 60)
+    time.sleep(aguarde_segundos)
