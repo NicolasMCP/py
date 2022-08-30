30ed094c058e085b68ed9a92cfa457316ba192b1
Nicolas Ramos <nicolas.mcp@gmail.com>
2022-08-30 14:22:38 -0300

    pprint() com dados

---
+++ lv-use-a-cabeca/cap_03g-Dic-Dic_p139.py
@@ -0,0 +1,23 @@
+# Barry, Paul. Use a Cabeça! Python (Página 137).  . Edição do Kindle.
+import pprint
+
+people = {}
+people['Ford'] = {'Name': 'Ford Prefect',
+                  'Gender': 'Male',
+                  'Occupation': 'Researcher',
+                  'Home Planet': 'Betelgeuse Seven'}
+people['Arthur'] = {'Name': 'Arthur Dent',
+                    'Gender': 'Male',
+                    'Occupation': 'Sandwich-Maker',
+                    'Home Planet': 'Earth'}
+people['Trillian'] = {'Name': 'Tricia McMillan',
+                      'Gender': 'Female',
+                      'Occupation': 'Mathematician',
+                      'Home Planet': 'Earth'}
+people['Robot'] = {'Name': 'Marvin',
+                   'Gender': 'Unknown',
+                   'Occupation': 'Paranoid Android',
+                   'Home Planet': 'Unknown'}
+print(people)
+print()
+pprint.pprint(people)
