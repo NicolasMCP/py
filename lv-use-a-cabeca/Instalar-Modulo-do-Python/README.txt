146955ae858f6379e31848818d6fa226c540e42c
Nicolas Ramos <nicolas.mcp@gmail.com>
2022-08-30 15:51:17 -0300

    Instalando um módulo no Python

---
+++ lv-use-a-cabeca/Instalar-Modulo-do-Python/README.txt
@@ -0,0 +1,4 @@
+# Nícolas Ramos
+# data: 6/8/2022
+# Livro Use a cabeça Python pág. 171
+# Código proprio solicitadas as modificações no livro.
---
+++ lv-use-a-cabeca/Instalar-Modulo-do-Python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='busque_letras',
+    version='1.0',
+    description="Devolve as letras (fornecidas) encontradas (em ordem alfabetica) na frase dada. Caso não seja fornecido o segundo argumento, ficara o default as vogais 'aeiou'.",
+    author='Nícolas Ramos',
+    author_email='nicolas.mcp@gmail.com',
+    url='https://github.com/NicolasMCP',
+    py_modules=['busque_letras'],
+)
+
