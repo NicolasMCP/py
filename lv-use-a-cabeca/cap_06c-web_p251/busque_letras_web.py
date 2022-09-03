16f54e6770bd9d1c76d9b6f2d5f0f43991deae2c
Nicolas Ramos <nicolas.mcp@gmail.com>
2022-09-03 12:10:56 -0300

    criando e visualizando LOG p_262

---
+++ lv-use-a-cabeca/cap_06c-web_p251/busque_letras_web.py
@@ -0,0 +1,48 @@
+import html
+from flask import Flask, render_template, request
+from busque_letras import busque_letras
+
+app = Flask(__name__)
+
+
+def log_request(req: 'request', res: str) -> None:
+    with open('busque_letras.log', 'a') as log:
+        # print(str(dir(req)), res, file=log)
+        print(str(req.form)[20:-2], file=log, end=' | ')
+        print(req.remote_addr, file=log, end=' | ')
+        # print(req.user_agent, file=log, end=' | ')
+        print(res, file=log, end='<br>')
+
+
+@app.route('/busca', methods=['POST'])
+def busca():
+    frase = request.form['frase']
+    letras = request.form['letras']
+    resultados = busque_letras(frase, letras)
+    subtitulo = 'Aqui estão seus resultados:'
+    titulo = 'Resultados'
+    log_request(request, resultados)
+    return render_template('resultados.html',
+                           frase=frase,
+                           letras=letras,
+                           subtitulo=subtitulo,
+                           titulo=titulo,
+                           resultados=resultados)
+
+
+@app.route('/')
+@app.route('/entrada')
+def entrada() -> 'html':
+    return render_template('entrada.html',
+                           subtitulo="Bem vindo ao 'busca_letras' na web")
+
+
+@app.route('/verlog')
+def ver_log() -> str:
+    with open('busque_letras.log') as log:
+        conteudo = log.read()
+        return conteudo
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
