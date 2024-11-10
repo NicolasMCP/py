10a684e4ab702ceb211f550702cb501eac929929
NicolasMCP <nicolas.mcp@gmail.com>
2024-11-10 19:56:25 -0300

    clear e operadores

---
+++ pythonando/introducao-py/clear_e_operadores.py
@@ -0,0 +1,22 @@
+import math
+import os
+# Clear - necessário importar 'os' 
+print("\n" * os.get_terminal_size().lines)
+#
+
+print(5 ** 5)
+
+print(15 % 3)
+
+print(17 // 5)
+print(16 // 5)
+print(15 // 5)
+
+print(12 ** 2)
+
+# raiz quadrada
+print(math.sqrt(144))
+
+# raiz quadrada usando a exponenciação 
+# (sem usar o math.sqrt) nem importar
+print(144 ** 0.5)
