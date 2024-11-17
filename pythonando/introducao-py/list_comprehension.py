9c483faebca992677cca6d917c5025cae067cca0
NicolasMCP <nicolas.mcp@gmail.com>
2024-11-17 13:27:05 -0300

    List Comprehension melhor formatado

--- pythonando/introducao-py/list_comprehension.py
+++ pythonando/introducao-py/list_comprehension.py
@@ -1,24 +1,24 @@
 import random
 
-# estou usando números inteiros apenas por didática
+print('usando números inteiros apenas por didática\n')
 
 print('\nExemplo 1\n')
 
 x = [f'5 x {i} = {i*5}' for i in range(1, 11)]
-print(x)
+print(*x,sep='\n')
 
 print('\nExemplo 2\n')
 
 y = [random.randint(500, 2000) for i in range(10)]
 print(y)
 
-# cobrar 90% de imposto nos que forem de 1000 ou mais
+print('Cobrar 90% de imposto nos que forem de 1000 ou mais...')
 l = [int(i * 1.9) if i >= 1000 else i for i in y]
 print(l)
 
 print('\nExemplo 3\n')
 
-# preserve somente os valores até 2500 usando a lista 'l'
+print('Preserve somente os valores até 2500 usando a última lista')
 z = [i for i in l if i <= 2500]
 print(z)
 
